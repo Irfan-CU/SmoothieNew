cf1577b83852582299542d2f14e791c30262baaf
Irfan <irfanmustafa312012@gmail.com>
2022-02-25 04:03:21 -0500

    Spring Intializer for CustomerFoodOrdering

---
+++ backEnd/CustomerFoodOrdering/src/test/java/com/backend/CustomerFoodOrdering/CustomerFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.CustomerFoodOrdering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
